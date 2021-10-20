7560861c86499b43d3fa84a24388efad2ba21e48
StuFF mc <m+git@carrascomolina.com>
2021-10-20 18:04:01 +0200

    Null passed to a callee that requires a non-null argument

--- WebRTCDemoSignalling/WebRTCDemo/third_party/SocketRocket/SRWebSocket.m
+++ WebRTCDemoSignalling/WebRTCDemo/third_party/SocketRocket/SRWebSocket.m
@@ -1744,7 +1744,7 @@ static NSRunLoop *networkRunLoop = nil;
         _runLoop = [NSRunLoop currentRunLoop];
         dispatch_group_leave(_waitGroup);
         
-        NSTimer *timer = [[NSTimer alloc] initWithFireDate:[NSDate distantFuture] interval:0.0 target:nil selector:nil userInfo:nil repeats:NO];
+        NSTimer *timer = [[NSTimer alloc] initWithFireDate:[NSDate distantFuture] interval:0.0 target:self selector:@selector(wait) userInfo:nil repeats:NO];
         [_runLoop addTimer:timer forMode:NSDefaultRunLoopMode];
         
         while ([_runLoop runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]]) {
@@ -1752,6 +1752,10 @@ static NSRunLoop *networkRunLoop = nil;
         }
         assert(NO);
     }
+}
+
+- (void)wait {
+    NSLog(@"Waiting...");
 }
 
 - (NSRunLoop *)runLoop;
